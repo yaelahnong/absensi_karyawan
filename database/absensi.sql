c7709a804166d1a45107260b35de7a1e6a370eaa
pupuoktavia26 <pupuoktaviaa@gmail.com>
2020-11-18 18:05:44 +0700

    t comming

--- database/absensi.sql
+++ database/absensi.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.9.0.1
+-- version 4.8.3
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 17, 2020 at 04:50 AM
--- Server version: 10.3.16-MariaDB
--- PHP Version: 7.3.7
+-- Generation Time: Nov 18, 2020 at 12:04 PM
+-- Server version: 10.1.36-MariaDB
+-- PHP Version: 7.2.11
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -44,10 +44,10 @@ CREATE TABLE `absen` (
 --
 
 INSERT INTO `absen` (`id_absen`, `jam_masuk`, `jam_keluar`, `keterangan`, `tanggal`, `id_user`, `created_at`, `updated_at`) VALUES
-(1, '08:24:09', '19:28:42', '', '2020-10-28', 32, '2020-11-03 08:27:32', NULL),
-(2, '08:27:46', '19:29:22', '', '2020-10-28', 28, NULL, NULL),
-(3, '08:30:09', '19:30:09', '', '2020-10-28', 31, NULL, NULL),
-(4, '09:01:55', '19:30:55', '', '2020-10-28', 29, NULL, NULL);
+(1, '08:24:09', '19:28:42', '', '2020-10-28', 35, '2020-11-03 08:27:32', NULL),
+(2, '08:27:46', '19:29:22', '', '2020-10-28', 38, '2020-11-17 17:00:00', NULL),
+(3, '08:30:09', '19:30:09', '', '2020-10-28', 33, '2020-11-17 17:00:00', NULL),
+(4, '09:01:55', '19:30:55', '', '2020-10-28', 37, '2020-11-17 17:00:00', NULL);
 
 -- --------------------------------------------------------
 
@@ -72,7 +72,9 @@ CREATE TABLE `admin` (
 
 INSERT INTO `admin` (`id_admin`, `nama`, `username`, `password`, `photo`, `id_akses`, `created_at`, `updated_at`) VALUES
 (20, 'Administrator', 'admin', '$2y$10$atJfVZK/hDkvAwh3Xm31a.op1RRKlsKe6OWvZMjCVoBWvmFGnEK8a', 'IMG_1089.jpg', 0, '2020-11-09 11:10:37', NULL),
-(21, 'Marino Imola', 'yaelahnong', '$2y$10$E3HaN2qQhmDnrRLAPNKqHudskW70n7mwiVWmBg0oXEzf0GJXV1LKq', 'ino.jpg', 1, '2020-11-09 11:28:38', NULL);
+(21, 'Marino Imola', 'yaelahnong', '$2y$10$E3HaN2qQhmDnrRLAPNKqHudskW70n7mwiVWmBg0oXEzf0GJXV1LKq', 'ino.jpg', 1, '2020-11-09 11:28:38', NULL),
+(22, 'pupu oktavia', 'Pupu', '$2y$10$YrFZNEirCu3onL.WoyG9K.awwdU0teZ95ws9OcsfhE6ZP9zOylplu', 'IMG20190119140943.jpg', 2, '2020-11-17 04:14:31', NULL),
+(23, 'pupu oktavia', 'PUPUP', '$2y$10$48.keCz0ZhNk6mcp1YgQTuSUJ6Pk3C4be2ChI2roBG0pkxiOHRFk.', 'IMG20181112183248.jpg', 3, '2020-11-18 03:14:53', NULL);
 
 -- --------------------------------------------------------
 
@@ -116,9 +118,10 @@ CREATE TABLE `department` (
 --
 
 INSERT INTO `department` (`id_department`, `ket_department`, `created_at`, `updated_at`) VALUES
-(1, 'Software Developement', '2020-11-09 18:41:57', NULL),
-(2, 'Digital Marketing', '2020-11-09 18:42:04', NULL),
-(3, 'Manage Service Provider', '2020-11-09 18:42:12', NULL);
+(1, 'Manage Service Provider', '2020-11-09 18:41:57', '2020-11-17 07:42:53'),
+(2, 'Digital Marketing', '2020-11-17 06:15:16', '2020-11-17 07:43:16'),
+(3, 'Software Development', '2020-11-17 06:15:35', '2020-11-17 06:15:48'),
+(5, 'IT Support', '2020-11-17 08:56:21', '2020-11-18 03:16:48');
 
 -- --------------------------------------------------------
 
@@ -162,7 +165,7 @@ CREATE TABLE `schedule` (
 --
 
 INSERT INTO `schedule` (`id_schedule`, `jam_masuk`, `jam_keluar`, `created_at`, `updated_at`) VALUES
-(1, '08:00:00', '19:00:00', '2020-11-09 18:35:29', NULL);
+(1, '11:00:00', '22:00:00', '2020-11-09 18:35:29', '2020-11-17 11:05:27');
 
 -- --------------------------------------------------------
 
@@ -177,8 +180,6 @@ CREATE TABLE `user` (
   `email` varchar(50) NOT NULL,
   `password` varchar(255) NOT NULL,
   `api_token` varchar(255) DEFAULT NULL,
-  `reset_password_token` varchar(255) DEFAULT NULL,
-  `reset_password_expires` datetime DEFAULT NULL,
   `jenis_kelamin` enum('laki-laki','perempuan') NOT NULL,
   `alamat` text NOT NULL,
   `no_telp` varchar(20) NOT NULL,
@@ -193,8 +194,11 @@ CREATE TABLE `user` (
 -- Dumping data for table `user`
 --
 
-INSERT INTO `user` (`id_user`, `nip`, `nama`, `email`, `password`, `api_token`, `reset_password_token`, `reset_password_expires`, `jenis_kelamin`, `alamat`, `no_telp`, `status`, `id_akses`, `id_department`, `created_at`, `updated_at`) VALUES
-(32, '200310032021061456', 'Marino Imola', 'marinoimola@gmail.com', '$2y$10$YFyasgl2.6md2BvMQVEriOhvFkBSSJJnb2jHDx5EgMeA.mX3xHWyG', 'bb51a85325739fa43791ef372f2d2d43f015fc2a', 'eyJpdiI6InlFRjlzWmo3OG5OQnQ1ak9YS2cxRUE9PSIsInZhbHVlIjoiZ2FtcHcrU1hoVFNSRUpHbUFWcWYrdz09IiwibWFjIjoiOGJjNGY5MWM4NGE1MzIzNjJlYjNkMDljZTY2ODYzN2ZlMzQ2YzA2NDhlNDAwZjg4NmVkMTBhOTI2ZWYxMzZmOCJ9', '2020-11-17 01:32:12', 'laki-laki', 'Jl.Cijahe no.1 rt02/rw01 kel.Curug Mekar kec.Bogor Barat Bogor 16113', '081284855532', 'Student', 2, 1, '2020-11-03 02:14:48', '2020-11-16 21:49:27');
+INSERT INTO `user` (`id_user`, `nip`, `nama`, `email`, `password`, `api_token`, `jenis_kelamin`, `alamat`, `no_telp`, `status`, `id_akses`, `id_department`, `created_at`, `updated_at`) VALUES
+(33, '26789135787129', 'pupu oktavia', 'pupuoktaviaa@gmail.com', '$2y$10$9BJNQfswUf/w6uycVBk3Tu2rGGY7CGvKgm9NV6C8hRolMzUDzWnQu', NULL, 'perempuan', 'kp cicadas 02 rt3 rw2', '0896712541', 'Student high', 2, 3, '2020-11-17 07:43:34', '2020-11-17 08:58:55'),
+(35, '200310032021061001', 'Marino Imola', 'marinoimola@gmail.com', '$2y$10$iJflnTAy5KXd.Hx8C4SDp.iNqRQiptnh0qQeOI28mwvFasybDLGCK', NULL, 'laki-laki', 'Jl.Cijahe no.1 rt02/rw01 kec.Bogor Barat kel.Curug Mekar Bogor 16113', '081284855532', 'Pelajar', 4, 1, '2020-11-17 08:49:14', '2020-11-17 08:50:54'),
+(37, '465765876465', 'Haikal Damar', 'haikaldamar23@gmail.com', '$2y$10$H5y0rAYFnY4r.WYLP7buFO.9esYB1XHRzpaRztopOE13Wa6ooX7fa', NULL, 'laki-laki', 'BTN Purwasari Regency blok B no 48 rt/rw 004/006 kecamatan cicurug kab.sukabumi ', '085722737371', 'Laku keras ', 4, 5, '2020-11-17 09:00:17', '2020-11-18 03:16:26'),
+(38, '2003090306', 'Rima Lestari', 'lrima989@gmail.com', '$2y$10$nffu.zmSfMIQLEFJFuXJiOZfRu7b5JYuBfGrYLNGD7jv5bDkDNT7y', NULL, 'perempuan', 'jln simpang tiga', '089614224096', 'Jomblo', 3, 2, '2020-11-17 09:04:42', NULL);
 
 --
 -- Indexes for dumped tables
@@ -256,13 +260,13 @@ ALTER TABLE `absen`
 -- AUTO_INCREMENT for table `admin`
 --
 ALTER TABLE `admin`
-  MODIFY `id_admin` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+  MODIFY `id_admin` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
 
 --
 -- AUTO_INCREMENT for table `department`
 --
 ALTER TABLE `department`
-  MODIFY `id_department` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id_department` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `overtime`
@@ -280,7 +284,7 @@ ALTER TABLE `schedule`
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id_user` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=38;
+  MODIFY `id_user` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=39;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
