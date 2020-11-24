930d769b64a4fe9995ecf65111e9334097f0954a
yaelahnong <marinoimola27@gmail.com>
2020-11-24 18:35:26 +0700

    export database baru

--- database/absensi.sql
+++ database/absensi.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 24, 2020 at 05:24 AM
+-- Generation Time: Nov 24, 2020 at 12:34 PM
 -- Server version: 10.3.16-MariaDB
 -- PHP Version: 7.3.7
 
@@ -35,6 +35,7 @@ CREATE TABLE `absen` (
   `keterangan` text DEFAULT NULL,
   `tanggal` date NOT NULL,
   `id_user` int(10) NOT NULL,
+  `koordinat` text NOT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
@@ -43,31 +44,31 @@ CREATE TABLE `absen` (
 -- Dumping data for table `absen`
 --
 
-INSERT INTO `absen` (`id_absen`, `jam_masuk`, `jam_keluar`, `keterangan`, `tanggal`, `id_user`, `created_at`, `updated_at`) VALUES
-(1, '08:24:09', '19:28:42', '', '2020-10-28', 32, '2020-11-03 08:27:32', NULL),
-(2, '08:27:46', '19:29:22', '', '2020-10-28', 39, '2020-11-17 17:00:00', NULL),
-(3, '08:30:09', '19:30:09', '', '2020-10-28', 40, '2020-11-17 17:00:00', NULL),
-(4, '09:01:55', '19:30:55', '', '2020-10-28', 41, '2020-11-17 17:00:00', NULL),
-(5, NULL, NULL, 'izin', '2020-10-29', 32, '2020-10-28 23:00:00', NULL),
-(6, NULL, NULL, 'alpa', '2020-10-29', 39, '2020-10-29 00:00:00', NULL),
-(7, '07:22:00', '19:12:05', '', '2020-10-29', 40, '2020-10-28 17:00:00', NULL),
-(8, '07:51:09', '19:08:26', '', '2020-10-29', 41, '2020-10-28 17:00:00', NULL),
-(9, '07:41:30', '19:31:15', '', '2020-10-30', 32, '2020-10-29 17:00:00', NULL),
-(10, '07:53:26', '11:12:05', '', '2020-10-30', 39, '2020-10-29 17:00:00', NULL),
-(11, NULL, NULL, 'izin', '2020-11-30', 40, '2020-10-29 17:00:00', NULL),
-(12, '09:09:00', '18:07:00', '', '2020-12-09', 32, NULL, NULL),
-(13, '07:09:00', '14:00:00', '', '2020-12-17', 39, NULL, NULL),
-(14, '08:08:00', '19:03:00', '', '2020-12-08', 40, NULL, NULL),
-(15, '09:13:00', '19:03:00', '', '2020-11-08', 41, NULL, NULL),
-(16, '09:13:00', '11:12:05', '', '2020-11-16', 39, NULL, NULL),
-(17, '09:13:00', '24:08:00', '', '2020-11-08', 40, NULL, NULL),
-(20, '08:25:00', NULL, NULL, '2020-11-24', 39, '2020-11-23 23:01:22', '2020-11-23 23:01:22'),
-(21, '08:25:00', NULL, NULL, '2020-11-24', 40, '2020-11-23 23:22:05', '2020-11-23 23:22:05'),
-(22, '08:25:00', NULL, NULL, '2020-11-24', 41, '2020-11-23 23:23:54', '2020-11-23 23:23:54'),
-(23, '08:25:00', NULL, NULL, '2020-11-24', 41, '2020-11-23 23:25:16', '2020-11-23 23:25:16'),
-(24, '08:25:00', NULL, NULL, '2020-11-24', 41, '2020-11-23 23:25:24', '2020-11-23 23:25:24'),
-(25, '08:25:00', NULL, NULL, '2020-11-24', 41, '2020-11-23 23:29:30', '2020-11-23 23:29:30'),
-(26, '07:25:00', '15:00:00', NULL, '2020-11-24', 32, '2020-11-24 00:29:48', '2020-11-24 00:39:17');
+INSERT INTO `absen` (`id_absen`, `jam_masuk`, `jam_keluar`, `keterangan`, `tanggal`, `id_user`, `koordinat`, `created_at`, `updated_at`) VALUES
+(1, '08:24:09', '19:28:42', '', '2020-10-28', 32, '', '2020-11-03 08:27:32', NULL),
+(2, '08:27:46', '19:29:22', '', '2020-10-28', 39, '', '2020-11-17 17:00:00', NULL),
+(3, '08:30:09', '19:30:09', '', '2020-10-28', 40, '', '2020-11-17 17:00:00', NULL),
+(4, '09:01:55', '19:30:55', '', '2020-10-28', 41, '', '2020-11-17 17:00:00', NULL),
+(5, NULL, NULL, 'izin', '2020-10-29', 32, '', '2020-10-28 23:00:00', NULL),
+(6, NULL, NULL, 'alpa', '2020-10-29', 39, '', '2020-10-29 00:00:00', NULL),
+(7, '07:22:00', '19:12:05', '', '2020-10-29', 40, '', '2020-10-28 17:00:00', NULL),
+(8, '07:51:09', '19:08:26', '', '2020-10-29', 41, '', '2020-10-28 17:00:00', NULL),
+(9, '07:41:30', '19:31:15', '', '2020-10-30', 32, '', '2020-10-29 17:00:00', NULL),
+(10, '07:53:26', '11:12:05', '', '2020-10-30', 39, '', '2020-10-29 17:00:00', NULL),
+(11, NULL, NULL, 'izin', '2020-11-30', 40, '', '2020-10-29 17:00:00', NULL),
+(12, '09:09:00', '18:07:00', '', '2020-12-09', 32, '', NULL, NULL),
+(13, '07:09:00', '14:00:00', '', '2020-12-17', 39, '', NULL, NULL),
+(14, '08:08:00', '19:03:00', '', '2020-12-08', 40, '', NULL, NULL),
+(15, '09:13:00', '19:03:00', '', '2020-11-08', 41, '', NULL, NULL),
+(16, '09:13:00', '11:12:05', '', '2020-11-16', 39, '', NULL, NULL),
+(17, '09:13:00', '24:08:00', '', '2020-11-08', 40, '', NULL, NULL),
+(20, '08:25:00', NULL, NULL, '2020-11-24', 39, '', '2020-11-23 23:01:22', '2020-11-23 23:01:22'),
+(21, '08:25:00', NULL, NULL, '2020-11-24', 40, '', '2020-11-23 23:22:05', '2020-11-23 23:22:05'),
+(22, '08:25:00', NULL, NULL, '2020-11-24', 41, '', '2020-11-23 23:23:54', '2020-11-23 23:23:54'),
+(23, '08:25:00', NULL, NULL, '2020-11-24', 41, '', '2020-11-23 23:25:16', '2020-11-23 23:25:16'),
+(24, '08:25:00', NULL, NULL, '2020-11-24', 41, '', '2020-11-23 23:25:24', '2020-11-23 23:25:24'),
+(25, '08:25:00', NULL, NULL, '2020-11-24', 41, '', '2020-11-23 23:29:30', '2020-11-23 23:29:30'),
+(26, '07:25:00', '15:00:00', NULL, '2020-11-24', 32, '-6.5646262, 106.7667172', '2020-11-24 00:29:48', '2020-11-24 00:39:17');
 
 -- --------------------------------------------------------
 
@@ -215,7 +216,7 @@ CREATE TABLE `user` (
 --
 
 INSERT INTO `user` (`id_user`, `nip`, `nama`, `email`, `password`, `api_token`, `reset_password_token`, `reset_password_expires`, `jenis_kelamin`, `alamat`, `no_telp`, `status`, `foto`, `id_akses`, `id_department`, `created_at`, `updated_at`) VALUES
-(32, '200310032021061456', 'Marino Imola', 'marinoimola@gmail.com', '$2y$10$2t4odd.RYIZQsLipKJw5O.XeBaIalE1IAWTgufDRrRzUwr436CkIm', '2a16d8fb505fcbea80a8c6d1a4b6da5b64d9543a', 'eyJpdiI6InVCTGFnb1N0Rmt0Z2tCc2gxT01WTkE9PSIsInZhbHVlIjoiQXltKzFNWndvTXVjNkRDV2dURGN0UT09IiwibWFjIjoiZjJmZWJiYzFlZDYzMjZlMDVlYTRjNmE0MzcwMTVhN2FiNGE4NTJjNTU1OTBiZWZlODEyZmY1MzVhY2IyY2M3MiJ9', '2020-11-23 20:43:34', 'laki-laki', 'Jl.Cijahe no.1 rt02/rw01 kel.Curug Mekar kec.Bogor Barat Bogor 16113', '081284855532', 'Student', 'ino.jpg', 2, 1, '2020-11-03 02:14:48', '2020-11-24 02:03:29'),
+(32, '200310032021061456', 'Marino Imola', 'marinoimola@gmail.com', '$2y$10$2t4odd.RYIZQsLipKJw5O.XeBaIalE1IAWTgufDRrRzUwr436CkIm', '8e3855f521f7c3222ebbc7efeb87365bcf3e2b2c', 'eyJpdiI6InVCTGFnb1N0Rmt0Z2tCc2gxT01WTkE9PSIsInZhbHVlIjoiQXltKzFNWndvTXVjNkRDV2dURGN0UT09IiwibWFjIjoiZjJmZWJiYzFlZDYzMjZlMDVlYTRjNmE0MzcwMTVhN2FiNGE4NTJjNTU1OTBiZWZlODEyZmY1MzVhY2IyY2M3MiJ9', '2020-11-23 20:43:34', 'laki-laki', 'Jl.Cijahe no.1 rt02/rw01 kel.Curug Mekar kec.Bogor Barat Bogor 16113', '081284855532', 'Student', 'ino.jpg', 2, 1, '2020-11-03 02:14:48', '2020-11-24 11:29:54'),
 (39, '2003090603', 'Rima Lestari', 'lrima989@gmail.com', '$2y$10$fytcNBcfpbC3AO3Ej9W/o.08sIl9Q4zlts6Fso5nwq1fx1OM20L0W', NULL, NULL, NULL, 'perempuan', 'Jln.simpang tiga', '089614224096', 'Jomblo', 'lumen-1-logo.png', 2, 1, '2020-11-19 03:45:15', '2020-11-23 12:53:40'),
 (40, '12365478909282', 'Pupu Oktavia', 'oktaviapupu@gmail.com', '$2y$10$gacKVZ28SBqo25lo.4DrXOBPkH6.PE1d9hXctTEscZu84dhuHb9pi', '3dfa53c8a0a847d40fd6456563bbd6dddcffb4d1', 'eyJpdiI6IlVvdmNIZzd6U3l5VUJTcHprQ3BVTVE9PSIsInZhbHVlIjoiZkplNzdIWW1nbGVlcSt3aGpVQzZOUT09IiwibWFjIjoiYWU3OTMwYmJhYzdlZWY4NTVkMDI3YTIyZGY1YTBjNzI1MGRjOGI0Zjc1OGQwYzBhMzM0MzgzZjY3YWQ2MThlZCJ9', '2020-11-19 19:43:04', 'perempuan', 'Jln. Cicadas 02 RT/RW 003/002 kecamatan ciampea kabupaten bogor 116620', '08958000000', 'Mau Nikah', '', 3, 3, '2020-11-19 03:46:34', '2020-11-19 12:10:13'),
 (41, '12039137131236123', 'Haikal Damar', 'haikaldamar23@gmail.com', '$2y$10$h5uTtKW.bX9j.jBzQBcXpOfKTTPpSY0Kxx.sMFW4XZiBdYKF6SqXm', '2df3afeaf6b0479c5f212d74451b42480696b18c', NULL, NULL, 'laki-laki', 'BTN Purwasari Regency Blok B no 48 RT/RW 004/006 Desa Purwasari Kec.Cicurug Kab.Sukabumi ', '085722737371', 'laku keras', 'damar.jpg', 2, 1, '2020-11-19 03:50:24', '2020-11-24 03:31:21');
